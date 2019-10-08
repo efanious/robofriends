f65cf578a3b56c5ed86516653f138bb92fb9b97b
Efana Usua <usuaefana@gmail.com>
2019-10-08 23:42:17 +0100

    Added scrolling and fonts, and finished up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import {robots} from './robots';
 import './App.css';
 
@@ -10,9 +11,15 @@ class App extends Component {
 	constructor(){
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield:''
 		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => this.setState({robots: users}));
 	}
 
 	onSearchChange = (event) => {
@@ -28,7 +35,9 @@ class App extends Component {
 			<div className='tc'>
 				<h1 className='f1'>RoboFriends</h1>
 				<SearchBox searchChange={this.onSearchChange}/>
-				<CardList robots={filteredRobots}/>
+				<Scroll>
+					<CardList robots={filteredRobots}/>
+				</Scroll>
 			</div>
 		);
 
